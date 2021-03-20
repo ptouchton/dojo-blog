49346d951fefaa3507f5ec2754d422d6e75a468d
Paul Touchton <p_touchton@yahoo.com>
2021-03-20 13:50:44 -0700

    home some button handler

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import {useState} from 'react';
+
 const Home = () => {
+
+    const [name, setName] = useState('Mario');
+    const [age, setAge] = useState(25);
+
+    const handleClick = (name) => {
+        setName('Luigi');
+        setAge(30)
+        // console.log(`Clicked: ${name}`);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{`${name} is ${age} years old`}</p>
+            <button onClick={() => handleClick('mario')}>Click me</button>
         </div>
      );
 }
