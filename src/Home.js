0a38f81953dbdad1a2d42ce01562f71b3c10faac
Paul Touchton <p_touchton@yahoo.com>
2021-03-20 18:01:23 -0700

    effects with dependency

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -9,14 +9,22 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = ((id) => {
-       const newBlog = blogs.filter((blog) => blog.id !== id);
-       setBlogs(newBlog);
+        const newBlog = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlog);
     })
+
+    useEffect(() => {
+        console.log('Effect ran');
+    }, [name]);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete}/>
+            <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
+            <button onClick={() => { setName('luigi') }}>Change Name</button>
+            <p>{name}</p>
         </div>
     );
 }
