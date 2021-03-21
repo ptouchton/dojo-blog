38a15a1afef2dc5edb91460e88ebd7b7ed8a1a2d
Paul Touchton <p_touchton@yahoo.com>
2021-03-20 18:29:38 -0700

    json server fetch

--- src/Home.js
+++ src/Home.js
@@ -3,28 +3,19 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-
-    const [name, setName] = useState('mario');
-
-    const handleDelete = ((id) => {
-        const newBlog = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlog);
-    })
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('Effect ran');
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+        .then((res) => {return res.json()})
+        .then((data) => {
+            setBlogs(data);
+        })
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
-            <button onClick={() => { setName('luigi') }}>Change Name</button>
-            <p>{name}</p>
+           { blogs && <BlogList blogs={blogs} title='All Blogs' />}
         </div>
     );
 }
