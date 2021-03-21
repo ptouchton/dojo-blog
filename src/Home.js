160520e6e8a102e66ba5e147d989dfb1d14caae0
Paul Touchton <p_touchton@yahoo.com>
2021-03-21 10:52:41 -0700

    simple error handling

--- src/Home.js
+++ src/Home.js
@@ -5,21 +5,33 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-                .then((res) => { return res.json() })
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error(`Server responded with error: ${res.status}`)
+                    }
+                    return res.json()
+                })
                 .then((data) => {
                     setBlogs(data);
                     setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
                 })
         }, 1000)
     }, []);
 
     return (
         <div className="home">
-            { isPending && <div>Loading...</div> }
+            { error && <div>{error}</div>}
+            { isPending && <div>Loading...</div>}
             { blogs && <BlogList blogs={blogs} title='All Blogs' />}
         </div>
     );
