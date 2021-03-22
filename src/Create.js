e3a92cdedcf630fae0de50b03675409699456024
Paul Touchton <p_touchton@yahoo.com>
2021-03-21 17:34:50 -0700

    adding create form

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState();
+    const [author, setAuthor] = useState();
+
+    return (
         <div className="create">
-            <h2>Create New Blog Entry</h2>
+            <label>Blog Title:</label>
+            <input
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            ></input>
+            <label>Blog Content</label>
+            <textarea
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+            ></textarea>
+            <label>Blog Author:</label>
+            <select
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+            >
+                <option>mario</option>
+                <option>yoshi</option>
+            </select>
+            
         </div>
-     );
+
+    );
 }
- 
+
 export default Create;
