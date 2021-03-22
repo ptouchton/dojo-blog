090a56ed82136a99366f2e22047a8ec51411856e
Paul Touchton <p_touchton@yahoo.com>
2021-03-21 19:48:18 -0700

    initial form updates

--- src/Create.js
+++ src/Create.js
@@ -6,27 +6,35 @@ const Create = () => {
     const [body, setBody] = useState();
     const [author, setAuthor] = useState();
 
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author}
+        console.log(blog);
+    }
+    
     return (
         <div className="create">
-            <label>Blog Title:</label>
-            <input
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            ></input>
-            <label>Blog Content</label>
-            <textarea
-                value={body}
-                onChange={(e) => setBody(e.target.value)}
-            ></textarea>
-            <label>Blog Author:</label>
-            <select
-                value={author}
-                onChange={(e) => setAuthor(e.target.value)}
-            >
-                <option>mario</option>
-                <option>yoshi</option>
-            </select>
-            
+            <form onSubmit={handleFormSubmit}>
+                <label>Blog Title:</label>
+                <input
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                ></input>
+                <label>Blog Content</label>
+                <textarea
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog Author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option>mario</option>
+                    <option>yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
 
     );
